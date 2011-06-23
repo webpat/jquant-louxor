5d1e5bc69901b999cf00f0b6af8a9013862ab65f
webpat <patrick.merheb@gmail.com>
2011-06-23 13:09:38 +1100

    Clean CSV

---
+++ src/main/java/org/jquant/data/louxor/tools/CleanCSV.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.jquant.data.louxor.tools;
+
+/**
+ * @author patrick.merheb
+ *
+ */
+public class CleanCSV {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+
+	}
+
+}
